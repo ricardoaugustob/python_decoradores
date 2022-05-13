9cc8a572a95c3f4426f3b1c971524433bb01ad63
ricardoaugustob <ricardoaugustob@outlook.com>
2022-05-13 07:43:25 -0300

    Commit 4

--- decoradores.py
+++ decoradores.py
@@ -1,8 +1,10 @@
 def validar(func):
     def valida(x,y):
+        #validação do numeros que vem da funcão soma
         if isinstance(x,int) and isinstance(y,int):
             return func(x,y)
         else:
+            # mensagem de erro caso algum numero seja letras ou diferente de int
             raise ValueError, print("Voce digitou letras ao invés de numeros")
     return valida
 
