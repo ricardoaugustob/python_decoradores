6b01ab4f08f7ba21b0e0ce7bb0163ef553229e09
ricardoaugustob <ricardoaugustob@outlook.com>
2022-05-13 07:33:06 -0300

    primeiro comitt

---
+++ decoradores.py
@@ -0,0 +1,14 @@
+def validar(func):
+    def valida(x,y):
+        if isinstance(x,int) and isinstance(y,int):
+            return func(x,y)
+        else:
+            raise ValueError, print("Voce digitou letras ao invés de numeros")
+    return valida
+
+@validar
+def soma(x,y):
+    return x+y
+
+
+print(soma(10,'ggg'))
